59829768b7b7eb9da0d1aabb5d6fc944a00ed836
arfat132 <arfatbegum90@gmail.com>
2022-06-02 19:52:48 +0300

    ser4ver setup

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const cors = require('cors')
+const jwt = require('jsonwebtoken');
+const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000
+require('dotenv').config()
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gsxbfwy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const foodsCollection = client.db("starbelly").collection("foods");
+
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Starbelly is running')
+})
+
+app.listen(port, () => {
+    console.log(`Starbelly app listening on port ${port}`)
+})
